07d4d7d72afeb48e556d53f9a9ed4b24eb189aeb
Lua19 <miguellua100@gmail.com>
2023-03-07 17:18:48 -0700

    Yo hice esto

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit{
 
   constructor() {}
 
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  
+  LoginEmail : string ='';
+  LoginPassword : string = '';
+
+  RegisterName : string='';
+  RegisterEmail : string ='';
+  RegisterPassword : string = '';
+
+
+  LoginSubmit() {
+
+    const Login = {
+      Email : this.LoginEmail,
+      Password : this.LoginPassword
+    }
+
+    console.log(Login);
+  }
+  RegisterSubmit(){
+    
+    const Register = {
+      Name : this.RegisterName,
+      Email : this.RegisterEmail,
+      Password : this.RegisterPassword
+    }
+    console.log(Register)
+  }
 }
